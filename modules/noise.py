2f2da49c1e02a1bb00c251dfa3623727eb61732f
Sudharsan S <ss0856@srmist.edu.in>
2026-03-02 23:31:37 +0530

    Update noise.py

--- modules/noise.py
+++ modules/noise.py
@@ -24,7 +24,7 @@ os.environ.setdefault("OPENBLAS_NUM_THREADS", "1")
 # SETTINGS
 # ------------------------------------------------------------
 
-STUDY_RADIUS      = 200
+STUDY_RADIUS      = 250
 GRID_RES          = 6
 ZOOM              = 19
 GROUND_ABSORPTION = 0.6
